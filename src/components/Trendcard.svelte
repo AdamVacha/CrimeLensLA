a72856b1c0839f150f974d001467242c0782c62f
unknown <gretel.arcia@yahoo.com>
2024-10-29 22:30:54 -0400

    homepage layout

--- src/components/Trendcard.svelte
+++ src/components/Trendcard.svelte
@@ -6,10 +6,10 @@
     export let buttonLink: string = '#'; // Optional link for the button
   </script>
   
-  <div class="hero bg-base-200 min-h-fit">
-    <div class="hero-content flex-col lg:flex-row max-w-4xl mx-auto">
-      <img src={imageSrc} alt={title} class="w-80 h-auto lg:w-96 rounded-lg shadow-2xl" />
-      <div class="lg:ml-16 mt-6 lg:mt-0 max-w-lg">
+  <div class="hero bg-base-200 min-h-fit max-w-10xl">
+    <div class="hero-content flex-col lg:flex-row max-w-100 mx-auto pt-20">
+      <img src={imageSrc} alt={title} class="w-100 h-auto lg:w-96 rounded-lg shadow-2xl" />
+      <div class="lg:ml-16 mt-6 lg:mt-0 max-w-5xl">
         <h1 class="text-2xl font-bold">{title}</h1>
         <p class="py-6">{description}</p>
         <a href={buttonLink}>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,23 +5,37 @@
 </script>
 <div
   class="hero min-h-screen"
-  style="background-image: url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp);">
+  style="background-image: url(https://images.unsplash.com/photo-1619678369125-8470fa23a979?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGxvcyUyMGFuZ2VsZXMlMjBza3lsaW5lfGVufDB8fDB8fHww);">
   <div class="hero-overlay bg-opacity-60"></div>
   <div class="hero-content text-neutral-content text-center">
     <div class="max-w-md">
-      <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
-      <p class="mb-5">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
-      <button class="btn btn-primary">Get Started</button>
+      <h1 class="mb-5 text-5xl font-bold fade-in-text" style="text-shadow: 7px 7px 2px #01030a;">
+        Welcome to CrimeLens LA
+      </h1>
     </div>
   </div>
 </div>
+
 <Trendcard
-  imageSrc="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp" 
-  title="Long-Term Trends" 
-  description="Track crime rates over the past years and compare crime categories such as theft, assault, and burglary. Adjust the time period and location to see how crime evolves in different regions" 
-  buttonText="Analyze Crime Trends" 
-  buttonLink="/long-term"
+    imageSrc="https://datawrapper.dwcdn.net/y5Sm6/full.png"
+    title="Long-Term Trends" 
+    description="Track crime rates over the past years and compare crime categories such as theft, assault, and burglary. Adjust the time period and location to see how crime evolves in different regions" 
+    buttonText="Analyze Crime Trends" 
+    buttonLink="/long-term"
 />
+<style>
+	@keyframes fadeIn {
+		from {
+			opacity: 0;
+			transform: translateY(20px);
+		}
+		to {
+			opacity: 1;
+			transform: translateY(0);
+		}
+	}
+
+	.fade-in-text {
+		animation: fadeIn 1.5s ease-out;
+	}
+</style>
