d0d370e4caaadaacfc7f2743a9bd091fb218611b
AdamVacha <adamrvacha@gmail.com>
2024-11-20 11:20:40 -0500

    updated page server with generic hard coded query to try and console log results, no luck yet

--- src/routes/demographic/+page.server.ts
+++ src/routes/demographic/+page.server.ts
@@ -3,13 +3,12 @@ import { connection } from '$lib/db';
 import { paramHelper } from '$lib/utils/search-param-helper.js';
 
 export async function load({ url }) {
-	const params = url.searchParams;
 	// common search params from paramHelper in /lib/utils
 	const commonParams = paramHelper(url.searchParams);
 
-	console.log(params.getAll('startDate'));
-
-	// TODO: implement queries
+	// TODO: implement demographic query
+	const result = await connection.execute('SELECT AGE FROM VICTIM WHERE ROWNUM < 10');
+	console.log(result);
 
 	// This data gets returned to the page component
 	return {
