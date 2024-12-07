eba7d924dd5fa8bdc84731c1d3e0f3ac2c898cfc
unknown <gretel.arcia@yahoo.com>
2024-12-06 22:14:02 -0500

    several UI fixes across all pages

--- src/routes/seasonal/+page.svelte
+++ src/routes/seasonal/+page.svelte
@@ -326,12 +326,11 @@
 </script>
 
 <form method="POST" onsubmit={handleSubmission}>
-	{JSON.stringify(formData)}
 	<div class="flex min-h-screen justify-center p-10 text-black">
-		<div class="max-w-8xl w-full rounded-lg bg-gray-100 p-10 pb-10 shadow-lg">
+		<div class="max-w-8xl w-full rounded-lg bg-gray-100 p-10 pb-20 shadow-lg">
 			<h1 class="mb-16 mt-8 text-center text-2xl font-semibold">Seasonal Crime Trends</h1>
 
-			<div class="grid grid-cols-1 gap-8 lg:grid-cols-[35%_62%]">
+			<div class="grid grid-cols-1 gap-8 lg:grid-cols-[28%_70%]">
 				<!-- Left Column: Controls -->
 				<div class="m-2 space-y-6">
 					<h2 class="text-grey text-grey-700 mb-2 text-lg">
@@ -424,11 +423,11 @@
 							<div
 								class="bg-grey-100/80 absolute inset-0 flex items-center justify-center backdrop-blur-sm"
 							>
-								<div class="text-center">
+								<div class="flex flex-col items-center">
 									<div
 										class="h-16 w-16 animate-spin rounded-full border-4 border-primary border-t-transparent"
 									></div>
-									<p class="text-grey-700 text-lg font-medium">Generating Chart...</p>
+									<p class="text-grey-700 mt-4 text-lg font-medium">Generating Chart...</p>
 								</div>
 							</div>
 						{/if}
