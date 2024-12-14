265aaff1054b85cfe9205ef3a61c6cbd5f07cefe
AdamVacha <adamrvacha@gmail.com>
2024-12-13 21:25:58 -0500

    finally working in oracle cloud db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,7 +4,10 @@ import { env } from '$env/dynamic/private';
 const dbconfig = {
 	user: env.ORACLE_USER,
 	password: env.ORACLE_PASSWORD,
-	connectString: env.ORACLE_CONNECTION_STRING
+	connectString: env.ORACLE_CONNECTION_STRING,
+	walletLocation: env.walletLocation,
+	walletPassword: env.walletPassword,
+	configDir: env.configDir
 };
 
 export const connection = await oracledb.getConnection(dbconfig);
